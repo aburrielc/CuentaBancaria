34f08993a1cea515dd1d637aa0c44cc7ba2261d5
alumno <alumno@aula01-02>
2016-11-22 17:53:57 +0100

    Casi acabado

--- AplicacionCuentaBancaria/src/aplicacioncuentabancaria/AplicacionCuentaBancaria.java
+++ AplicacionCuentaBancaria/src/aplicacioncuentabancaria/AplicacionCuentaBancaria.java
@@ -9,37 +9,67 @@ public class AplicacionCuentaBancaria {
         boolean nombreValido = false;
         
         if(nombre.length() <= 10){
+            System.out.println("Nombre de titular válido");
             nombreValido = true;
         }else{
+            System.out.println("Nombre de titular erróneo");
             nombreValido = false;
         }
         
         return nombreValido;
     }
     
-    public static String calculoDigitosControl(String entidad, String oficina, String numCompleto){
-        int vFactores[] = new int []{1, 2, 4, 8, 5, 10, 9, 7, 3, 6};
-        int resultadoComprobacion = 0;
-        String codigoComprobacion = "00" + entidad + oficina;
+    public static String calculoDigitosControl(String entidad, String oficina, String cuenta){
+        int vFactores[] = new int []{6, 3, 7, 9, 10, 5, 8, 4, 2, 1};
+        String codigoComprobacion = entidad + oficina;
+        String digitosControl = "";
+        int sumaCodigo = 0;
+        int resto = 0;
         
         for(int i=0;i<codigoComprobacion.length();i++){
-            
+            sumaCodigo += Integer.parseInt(codigoComprobacion.substring(i)) * vFactores[7 - i];
         }
+        
+        resto = sumaCodigo % 11;
+        digitosControl += String.valueOf(11 - resto);
+        
+        sumaCodigo = 0;
+        resto = 0;
+        
+        for(int i=0;i<cuenta.length();i++){
+            sumaCodigo += Integer.parseInt(cuenta.substring(i)) * vFactores[9 - i];
+        }
+        
+        resto = sumaCodigo % 11;
+        digitosControl += String.valueOf(11 - resto);
+        
+        return digitosControl;
     }
     
-    public static void validarCodigo(String ccc){
-        String vDigitos[] = new String[4];
-        String codigo = ccc.trim();
+    public static boolean validarCodigo(String vDigitos[]){
+        int extension = 0;
+        String comprobacionDigitos = "";
+        boolean valido = false;
         
-        if(codigo.length() == 10){
-            vDigitos[0] = codigo.substring(0, 3);
-            vDigitos[1] = codigo.substring(4, 7);
-            vDigitos[2] = codigo.substring(8, 9);
-            vDigitos[3] = codigo.substring(10, 19);
+        for(int i=0;i<vDigitos.length;i++){
+            extension += vDigitos[i].length();
+        }
+        
+        if(extension < 20){
             
+            System.out.println("Extensión de código errónea");
             
         }else{
-            System.out.println("Extensión de código errónea");
+            
+            comprobacionDigitos = calculoDigitosControl(vDigitos[0],vDigitos[1],vDigitos[3]);
+            
+            if(comprobacionDigitos.equals(vDigitos[2])){
+                System.out.println("El Código de Control es correcto");
+                valido = true;
+            }else{
+                System.out.println("El Código de Control es erróneo");
+                valido = false;
+            } 
         }
         
         /*Scanner digitos = new Scanner(ccc);
@@ -53,14 +83,18 @@ public class AplicacionCuentaBancaria {
             }
         }
         */
-         
+        return valido;
     }
 
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         String nombre = "";
         String ccc = "";
+        
+        String vDigitos[] = new String[4];
+        
         boolean comprobacionNombre = false;
+        boolean comprobacionCuenta = false;
         
         do{
            System.out.println("Introduzca el nombre del titular de la cuenta");
@@ -68,18 +102,22 @@ public class AplicacionCuentaBancaria {
            
            comprobacionNombre = comprobarNombre(nombre);
            
-           if(comprobacionNombre == false){
-               System.out.println("Nombre de titular no válido");
-           }else{
-               System.out.println("Nombre de titular válido");
-           }
-           
         }while(comprobacionNombre == false);
         
-        System.out.println("Introduzca el CCC de la cuenta completo");
-        ccc = leer.next();
+        do{
+            System.out.println("Introduzca los dígitos de ENTIDAD");
+            vDigitos[0] = leer.next();
+            System.out.println("Introduzca los dígitos de OFICINA");
+            vDigitos[1] = leer.next();
+            System.out.println("Introduzca los dígitos de CONTROL");
+            vDigitos[2] = leer.next();
+            System.out.println("Introduzca los dígitos de CUENTA");
+            vDigitos[3] = leer.next();
+
+            comprobacionCuenta = validarCodigo(vDigitos);
+            
+        }while(comprobacionCuenta == false);
         
-        validarCodigo(ccc);
     }
     
 }
